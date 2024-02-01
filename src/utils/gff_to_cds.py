26ac1157cafc3d0600422f6f326447288f51f377
AmirUCR <amohs002@ucr.edu>
2024-01-31 17:57:07 -0800

    Generates GFF not from orthogroups but for all species

--- src/utils/gff_to_cds.py
+++ src/utils/gff_to_cds.py
@@ -6,73 +6,69 @@ from utils.mycocosm_gff_to_cds import mycocosm_gff_to_cds
 import os
 import pandas as pd
 
+
 # ALL PATHS ARE RELATIVE TO THE ROOT DIRECTORY
 def create_cds_from_gff():
-    if not os.path.exists('data/fourdbs_concat/cds_from_gff'):
-        os.mkdir('data/fourdbs_concat/cds_from_gff')
-
-    out = 'data/fourdbs_concat/cds_from_gff'
-
-    df = pd.read_csv('data/fourdbs_concat/fourdbs_hi_input_species.csv')
-
-    ncbi_base_path = 'data/NCBI'
-    fdb_base_path = 'data/FungiDB'
-    ensembl_base_path = 'data/EnsemblFungi'
-    mycocosm_base_path = 'data/MycoCosm'
+    sources = ['NCBI', 'FungiDB', 'EnsemblFungi', 'MycoCosm']
+    base_paths = ['data/NCBI', 'data/FungiDB', 'data/EnsemblFungi', 'data/MycoCosm']
+    manifests = ['ncbi_input_species.csv', 'fungidb_input_species.csv', 'ensemblfungi_input_species.csv', 'mycocosm_input_species.csv']
 
     losers = list()
 
-    for idx, row in df.iterrows():
-        if idx % 100 == 0:
-            print(f'Done with {idx} rows...')
+    count = 0
+    for m_idx, m in enumerate(manifests):
+        csv_path = os.path.join(base_paths[m_idx], m)
 
-        src = row['source']
-        original_name = row['original_name']
-        cds_file_name = row['cds_file_name']
-        genome_file_name = row['genome_file_name']
-        gff_file_name = row['gff_file_name']
+        df = pd.read_csv(csv_path)
+        for _, row in df.iterrows():
+            if count % 100 == 0:
+                print(f'Done with {count} species...')
 
-        if os.path.exists(os.path.join(out, original_name + '_cds_from_gff.fna')):
-            print(os.path.join(out, original_name + '_cds_from_gff.fna'), 'exists. Skipping.')
-            continue
+            src = sources[m_idx]
+            src_base_path = base_paths[m_idx]
 
-        response = ''
+            original_name = row['original_name']
+            cds_file_name = row['cds_file_name']
+            genome_file_name = row['genome_file_name']
+            gff_file_name = row['gff_file_name']
 
-        if src == 'NCBI':
-            response = ncbi_gff_to_cds(original_name,
-                                    f'src/utils/ortholog_finder/orthogroups/{cds_file_name}',
-                                    f'{ncbi_base_path}/genomes/{genome_file_name}',
-                                    f'{ncbi_base_path}/gff/{gff_file_name}',
-                                    out)
-        elif src == 'FungiDB':
-            response = fungidb_gff_to_cds(original_name,
-                                        f'src/utils/ortholog_finder/orthogroups/{cds_file_name}',
-                                        f'{fdb_base_path}/genomes/{genome_file_name}',
-                                        f'{fdb_base_path}/gff/{gff_file_name}',
-                                        out)
-        elif src == 'EnsemblFungi':
-            response = ensembl_gff_to_cds(original_name,
-                                        f'src/utils/ortholog_finder/orthogroups/{cds_file_name}',
-                                        f'{ensembl_base_path}/genomes/{genome_file_name}',
-                                        f'{ensembl_base_path}/gff/{gff_file_name}',
-                                        out)
-        elif src == 'MycoCosm':
-            response = mycocosm_gff_to_cds(original_name,
-                                        f'src/utils/ortholog_finder/orthogroups/{cds_file_name}',
-                                        f'{mycocosm_base_path}/genomes/{genome_file_name}',
-                                        f'{mycocosm_base_path}/gff/{gff_file_name}',
-                                        out)
+            if os.path.exists(os.path.join(f'data/{src}/cds_from_gff/', original_name + '_cds_from_gff.fna')):
+                print(os.path.join(f'data/{src}/cds_from_gff/', original_name + '_cds_from_gff.fna'), 'exists. Skipping.')
+                count += 1
+                continue
+
+            response = ''
+
+            if src == 'NCBI':
+                response = ncbi_gff_to_cds(original_name,
+                                        f'{src_base_path}/cds/{cds_file_name}',
+                                        f'{src_base_path}/genomes/{genome_file_name}',
+                                        f'{src_base_path}/gff/{gff_file_name}')
+            elif src == 'FungiDB':
+                response = fungidb_gff_to_cds(original_name,
+                                            f'{src_base_path}/cds/{cds_file_name}',
+                                            f'{src_base_path}/genomes/{genome_file_name}',
+                                            f'{src_base_path}/gff/{gff_file_name}')
+            elif src == 'EnsemblFungi':
+                response = ensembl_gff_to_cds(original_name,
+                                            f'{src_base_path}/cds/{cds_file_name}',
+                                            f'{src_base_path}/genomes/{genome_file_name}',
+                                            f'{src_base_path}/gff/{gff_file_name}')
+            elif src == 'MycoCosm':
+                response = mycocosm_gff_to_cds(original_name,
+                                            f'{src_base_path}/cds/{cds_file_name}',
+                                            f'{src_base_path}/genomes/{genome_file_name}',
+                                            f'{src_base_path}/gff/{gff_file_name}')
+            
+            if response != 'OK':
+                print(f'{src}: No records found for {src_base_path}/cds/{cds_file_name}. Skipping.')
+                losers.append(original_name)
+            
+            count += 1
         
-        if response != 'OK':
-            print(f'No records found for {original_name}. Skipping.')
-            losers.append(original_name)
+        df = df[~df['original_name'].isin(losers)]
+        df['cds_file_name'] = df['cds_file_name'].str.replace('_cds', '_cds_from_gff')
+        df.to_csv(f'data/{src}/{src.lower()}_gff_input_species.csv', index=False)
 
     print()
     print('Losers:', losers)
-
-    df = df[~df['original_name'].isin(losers)]
-    df['cds_file_name'] = df['cds_file_name'].str.replace('_cds', '_cds_from_gff')
-    df.to_csv('data/fourdbs_concat/fourdbs_hi_gff_input_species.csv', index=False)
-
-    print('Created CDS files under data/fourdbs_concat/cds_from_gff. Place these in the input folder of ALLEGRO and point to the folder in config.yaml')
-    print('Created data/fourdbs_concat/fourdbs_hi_gff_input_species.csv. Use this as the input species csv for ALLEGRO.')
