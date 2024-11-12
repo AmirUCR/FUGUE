24bdfcd64ce3ccaf654d54a331d1ae8d74785204
AmirUCR <amohs002@ucr.edu>
2024-11-11 19:25:37 -0800

    Fix env name

--- src/utils/diamond/0_just_run_this.sh
+++ src/utils/diamond/0_just_run_this.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 conda run -n fugue python 1_create_sh_script.py
 conda run -n fugue python 2_make_proteome.py
-conda run -n diamond2 ./3_makedb_for_ref_species.sh
-conda run -n diamond2 ./4_makedb_for_all_species.sh
-conda run -n diamond2 ./5_run_bidirectional_diamond.sh
+conda run -n diamond ./3_makedb_for_ref_species.sh
+conda run -n diamond ./4_makedb_for_all_species.sh
+conda run -n diamond ./5_run_bidirectional_diamond.sh
 conda run -n fugue python 6_aggregate_orthogroups.py
